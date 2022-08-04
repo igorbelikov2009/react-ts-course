539bf2e3123c3ab1b672f2f72878de8541bb1001
Igor Belikov <igorbelikov2009@yandex.ru>
2022-08-04 13:51:50 +0300

    UserItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card, { CardVariant } from "./components/Card";
+import UserList from "./components/UserList";
+import { IUser } from "./types/types";
 
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    fetchUsers(users);
+  }, []);
+
+  async function fetchUsers(params: IUser[]) {
+    try {
+      const response = await axios.get<IUser[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+      console.log(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card variant={CardVariant.outlined} width="200px" height="200px">
+        <button>Кнопка</button>
+        <div>asfasf</div>
+      </Card>
+      <UserList users={users} />
     </div>
   );
 }
